1e8ec22bd03cb5052c31ad5ce944b1f5d53d2bcc
YeonheeHan <jess0884@naver.com>
2022-07-17 22:58:19 +0900

    스토리보드 레퍼런스를 추가합니다

---
+++ Dear/Dear/ThirdViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdViewController.swift
+//  Dear
+//
+//  Created by 한연희 on 2022/07/17.
+//
+
+import SwiftUI
+
+struct ThirdViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdViewController()
+    }
+}
