6fa095e43cc26f37688e27d73e60d8ff8ab9479f
YeonheeHan <jess0884@naver.com>
2022-07-18 00:16:52 +0900

    UI View 짜기

--- Dear/Dear/LetterViewController.swift
+++ Dear/Dear/LetterViewController.swift
@@ -14,6 +14,17 @@ class LetterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func PrintAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "마음카드를 보낼까요?", message: "발송 완료 후 수정할 수 없습니다", preferredStyle: .alert)
+        let yes = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let no = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+        
+        alert.addAction(no)
+        alert.addAction(yes)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
