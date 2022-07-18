de0fc8eaaca180bb464e0f84bb3e3afce152ac00
YeonheeHan <jess0884@naver.com>
2022-07-18 11:30:09 +0900

    Alert, Picker View 추가

--- Dear/Dear/HospitalViewController.swift
+++ Dear/Dear/HospitalViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class HospitalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var hospitalPickerView: UIPickerView!
+    
+    let userDefaults = UserDefaults.standard
+    let pickerViewSelectList = ["병원01", "병원02", "병원03"]
+    var selectedPicker: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        /* let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        */
+                
+        self.hideKeyboardWhenTappedAround()
     }
 
 
