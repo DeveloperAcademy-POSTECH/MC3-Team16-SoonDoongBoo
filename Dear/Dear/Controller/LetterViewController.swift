b01e13569423f06a21ca31405f7e89421a596c4c
YeonheeHan <jess0884@naver.com>
2022-07-18 15:39:20 +0900

    #6 아키텍처 수정
    MVC

--- Dear/Dear/Controller/LetterViewController.swift
+++ Dear/Dear/Controller/LetterViewController.swift
@@ -11,6 +11,7 @@ class LetterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
     }
 
