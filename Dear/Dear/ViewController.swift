f00e9d98fa09a9cf5d490cf8ce878ff9617bcae2
lau0505 <103024956+lau0505@users.noreply.github.com>
2022-07-18 19:35:40 +0900

    feat: #16 사용자 선택뷰 + 사용자 선택뷰 구현

--- Dear/Dear/ViewController.swift
+++ Dear/Dear/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SelectionnurseView: UIView!
+    @IBOutlet weak var SelectionpatientView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        SelectionnurseView.layer.cornerRadius = 10
+        SelectionpatientView.layer.cornerRadius = 10
+        SelectionnurseView.layer.borderColor = UIColor(named: "ButtonBorderColor")?.cgColor
+        SelectionpatientView.layer.borderColor = UIColor(named: "ButtonBorderColor")?.cgColor
+        SelectionnurseView.layer.borderWidth = 2
+        SelectionpatientView.layer.borderWidth = 2
+        
+        
+        let nuresTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewSelectionnurseTapped))
+        SelectionnurseView.addGestureRecognizer(nuresTapGestureRecognizer)
+        
+        let patientTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(viewSelectionpatientTapped))
+        SelectionpatientView.addGestureRecognizer(patientTapGestureRecognizer)
     }
-
-
+    
+    
+    @objc func viewSelectionnurseTapped(sender: UITapGestureRecognizer) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "vc1") as? ViewController1 else {return}
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func viewSelectionpatientTapped(sender: UITapGestureRecognizer) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "vc2") as? ViewController2 else {return}
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
-
---
+++ Dear/Dear/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  Dear
+//
+//  Created by 전호정 on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
