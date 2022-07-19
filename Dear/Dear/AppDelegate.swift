6b77f6bcb8fed2635fa1359743618ac8842e55bd
jayden000106 <cars0106@icloud.com>
2022-07-19 15:35:56 +0900

    feat: Firebase 익명로그인 구현
    - 중복된 패키지 dependency 제거

--- Dear/Dear/AppDelegate.swift
+++ Dear/Dear/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Auth.auth().signInAnonymously { authResult, error in
+            guard let user = authResult?.user else { return }
+            let uid = user.uid
+            print(uid)
+        }
         return true
     }
 
