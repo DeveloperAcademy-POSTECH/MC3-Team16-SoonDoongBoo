ca10d30b38652c723eb86b8d4f78af23d7e2a0ea
jimin <cjjunn0827@gmail.com>
2022-07-20 22:29:50 +0900

    feat: #13 간호사 메인 뷰 - UI 구현

---
+++ Dear/Dear/Cell/DDayCustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DDayCustomTableViewCell.swift
+//  Dear
+//
+//  Created by Jeon Jimin on 2022/07/19.
+//
+
+import UIKit
+
+class DDayCustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var DDayTitleLabel: UILabel!
+    @IBOutlet weak var DDayDateLabel: UILabel!
+    @IBOutlet weak var cellView: UIView!
+}
---
+++ Dear/Dear/Cell/LetterCustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  Dear
+//
+//  Created by Jeon Jimin on 2022/07/19.
+//
+
+import UIKit
+
+class LetterCustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var letterTitleLabel: UILabel!
+    @IBOutlet weak var letterContentLabel: UILabel!
+    @IBOutlet weak var letterDateLabel: UILabel!
+    @IBOutlet weak var cellView: UIView!
+}
