70213945263f2bc0ac5253a75c99d5f9d1a90b36
jayden000106 <cars0106@icloud.com>
2022-07-20 17:26:33 +0900

    wip

---
+++ Dear/Dear/Controllers/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Dear
+//
+//  Created by 정지혁 on 2022/07/20.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dear/Dear/Extentsions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  Dear
+//
+//  Created by 정지혁 on 2022/07/20.
+//
+
+import Foundation
