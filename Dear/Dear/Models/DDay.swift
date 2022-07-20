99560357c432577e9cd8752d935ca36a2d518acb
jimin <cjjunn0827@gmail.com>
2022-07-21 01:24:41 +0900

    fix: 충돌 방지를 위한 래퍼런스 삭제

--- Dear/Dear/Models/DDay.swift
+++ Dear/Dear/Models/DDay.swift
@@ -15,9 +15,11 @@ struct DDay: Identifiable {
 
 #if DEBUG
 extension DDay {
+    //12글자까지 화면에 표시됨
     static var sampleData = [
         DDay(title: "월급 받는 날", date: Date()),
-        DDay(title: "간호사 된 지", date: Date())
+        DDay(title: "간호사 된 지", date: Date()),
+        DDay(title: "가나다라마바사아자차카타파하", date: Date())
     ]
 }
 #endif
