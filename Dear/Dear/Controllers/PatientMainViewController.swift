5bef61440063f8bb6e7e8c33a63f076a219e6c4b
jayden000106 <cars0106@icloud.com>
2022-07-23 02:02:24 +0900

    wip
    - 마음카드 쓰는 뷰 연결

--- Dear/Dear/Controllers/PatientMainViewController.swift
+++ Dear/Dear/Controllers/PatientMainViewController.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class PatientMainViewController: UIViewController {
+    private var hospitalName = UserDefaults.standard.string(forKey: "hospital")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 우측 상단 네비게이션 아이템 설정 코드
         navigationItem.hidesBackButton = true
-        let sendLetterButton = UIBarButtonItem(image: UIImage(systemName: "square.and.pencil"), style: .plain, target: self, action: nil)
+        let sendLetterButton = UIBarButtonItem(image: UIImage(systemName: "square.and.pencil"), style: .plain, target: self, action: #selector(sendLetterButtonPressed))
         sendLetterButton.tintColor = .black
         navigationItem.setRightBarButton(sendLetterButton, animated: true)
+        
+        hospitalName = UserDefaults.standard.string(forKey: "hospital")
     }
-
-
+    
+    @objc func sendLetterButtonPressed() {
+        guard let letterViewController = storyboard?.instantiateViewController(withIdentifier: "LetterView") as? LetterViewController else {
+            return
+        }
+        navigationController?.pushViewController(letterViewController, animated: true)
+    }
 }
