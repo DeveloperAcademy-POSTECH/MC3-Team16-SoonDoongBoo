da8ed450f7fccfd71b9661edfe1121a5df2f1588
Hyunmin Cho <chm49@naver.com>
2022-07-26 16:06:54 +0900

    feat: #17 레이블 및 편지쓰기 버튼 배치

---
+++ Dear/Dear/Controllers/PatientPillBagViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PatientPillBagViewController.swift
+//  Dear
+//
+//  Created by Tempnixk on 2022/07/26.
+//
+
+import UIKit
+
+class PatientPillBagViewController: UIViewController {
+    @IBOutlet weak var pillBag: UIStackView!
+    
+    @IBOutlet weak var sentPrescription: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
