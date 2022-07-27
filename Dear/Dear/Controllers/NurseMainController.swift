b3d713fba0c104dbb467d82c1b7d43eee0377af5
jimin <cjjunn0827@gmail.com>
2022-07-27 18:14:52 +0900

    feat: #39 간호사 메인 뷰 - UI - mood view UI

--- Dear/Dear/Controllers/NurseMainController.swift
+++ Dear/Dear/Controllers/NurseMainController.swift
@@ -12,15 +12,28 @@ class NurseMainController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var moodView: UIView!
     
-    let moodSelectView = SelectMoodView()
+    let moodSelect = SelectMood()
+    let moodText = MoodText()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        moodView.addSubview(moodSelectView)
-//
-//        moodSelectView.center = moodView.convert(moodView.center, from:moodView.superview)
-        moodSelectView.frame.size = CGSize(width: moodView.frame.width, height: moodView.frame.height)
+        
+        //TODO: 추후 color extension 색으로 변경 필요
+        moodView.layer.cornerRadius = 10
+        moodView.layer.shadowColor = UIColor.gray.cgColor
+        moodView.layer.shadowOpacity = 0.25
+        moodView.layer.shadowRadius = 8
+        
+        moodSelect.frame.size = CGSize(width: moodView.frame.width, height: moodView.frame.height)
+//        moodView.addSubview(moodSelect)
+        moodView.addSubview(moodText
+        )
+        moodSelect.center = moodView.convert(moodView.center, from:moodView.superview)
+        
+        moodSelect.frame = moodView.bounds
+        moodSelect.translatesAutoresizingMaskIntoConstraints = true
+        moodSelect.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
     }
 
 }
---
+++ Dear/Dear/XibUIViewExtension.swift
@@ -0,0 +1,38 @@
+//
+//  XibUIViewExtension.swift
+//  Dear
+//
+//  Created by Jeon Jimin on 2022/07/27.
+//
+
+import UIKit
+
+//출처: https://poky-develop.tistory.com/16
+class XibUIView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        let className = String(describing: type(of: self))
+        guard let view = self.loadViewFromNib(nibName: className) else {
+            return
+        }
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+}
+
+extension UIView {
+    func loadViewFromNib(nibName: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
