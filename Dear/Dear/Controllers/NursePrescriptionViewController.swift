5512b426ee1560d8e4b95e6eb631002e75b8bee8
jimin <cjjunn0827@gmail.com>
2022-07-28 23:27:43 +0900

    feat: #55 간호사 처방전 뷰 - UI 구현

---
+++ Dear/Dear/Controllers/NursePrescriptionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NursePrescriptionView.swift
+//  Dear
+//
+//  Created by Jeon Jimin on 2022/07/28.
+//
+
+import UIKit
+
+class NursePrescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
