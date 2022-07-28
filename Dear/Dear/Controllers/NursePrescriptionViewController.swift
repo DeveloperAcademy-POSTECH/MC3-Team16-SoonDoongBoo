7941f9a9550d0ffddd2fd44b8db361b586b590c7
jimin <cjjunn0827@gmail.com>
2022-07-29 00:00:06 +0900

    feat: #55 간호사 처방전 뷰 - 샘플 데이터 연결 - 처방된 약만 화면에 띄우기

--- Dear/Dear/Controllers/NursePrescriptionViewController.swift
+++ Dear/Dear/Controllers/NursePrescriptionViewController.swift
@@ -8,8 +8,35 @@
 import UIKit
 
 class NursePrescriptionViewController: UIViewController {
+    
+    //TODO: 서버 데이터 받아와서 연결하기
+    let letterSampleData = Letter.sampleData[2]
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    @IBOutlet weak var depressionView: UIView!
+    @IBOutlet weak var sadnessView: UIView!
+    @IBOutlet weak var angerView: UIView!
+    @IBOutlet weak var calmView: UIView!
+    @IBOutlet weak var joyfulView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateLabel.text = letterSampleData.date
+        nameLabel.text = letterSampleData.letterTo
+        contentLabel.text = letterSampleData.letterContent
+        
+        setMedicine()
+    }
+    
+    private func setMedicine() {
+        depressionView.isHidden = letterSampleData.depression ? false : true
+        sadnessView.isHidden = letterSampleData.sadness ? false : true
+        angerView.isHidden = letterSampleData.anger ? false : true
+        calmView.isHidden = letterSampleData.calm ? false : true
+        joyfulView.isHidden = letterSampleData.joyful ? false : true
     }
 }
